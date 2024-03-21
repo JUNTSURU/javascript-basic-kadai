46ca50d47d44c2ff3a356ea14eb6095d7f50a6c2
JUNTSURU <132868464+JUNTSURU@users.noreply.github.com>
2024-03-21 11:18:22 +0900

    課題提出（mdn再提出）

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -4,7 +4,7 @@ const getTodayDate = () =>{
   const year = today.getFullYear();
   const month = today.getMonth();
   const date = today.getDate();
-  console.log(year+'年'+ (month+1) +'月',date+'日');
+  console.log(year+'年'+ (month+1) +'月'+date+'日');
 }
 
 getTodayDate ();
